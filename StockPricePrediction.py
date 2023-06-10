ecc53f74049e7527b6e3dce7d33512ba0c548042
KankaraAkashReddy <114745193+KankaraAkashReddy@users.noreply.github.com>
2023-06-10 08:16:39 +0530

    Update StockPricePrediction.py

--- StockPricePrediction.py
+++ StockPricePrediction.py
@@ -1,8 +1,9 @@
 import pandas_datareader as pdr
+import pandas as pd
 
+#Loading Google Stock Price data till date using tiingo
 df = pdr.get_data_tiingo('GOOG',api_key='303ab54719a6a63dd1e57c36916695053da99b86')
 df.to_csv('GOOG.csv')
-import pandas as pd
 
 df = pd.read_csv('GOOG.csv')
 print(df.head())
