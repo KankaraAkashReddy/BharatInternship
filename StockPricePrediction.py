c68967f99727f0697064694be25fcdc901f66f1b
KankaraAkashReddy <114745193+KankaraAkashReddy@users.noreply.github.com>
2023-06-10 15:54:07 +0530

    Update StockPricePrediction.py

--- StockPricePrediction.py
+++ StockPricePrediction.py
@@ -8,3 +8,17 @@ df.to_csv('GOOG.csv')
 df = pd.read_csv('GOOG.csv')
 print(df.head())
 print(df.tail())
+
+df1 = df.reset_index()['close']
+print(df1.shape)
+
+import matplotlib.pyplot as plt
+
+plt.plot(df1)
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+df1 = scaler.fit_transform(np.array(df1).reshape(-1,1))
+
+df1.shape
